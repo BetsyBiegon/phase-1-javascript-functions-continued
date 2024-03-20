d27ea62494ad74ba17bca5e0e714521ab871afc0
BetsyBiegon <betsybiegon@gmail.com>
2024-03-20 10:08:36 +0300

    cpmplete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// index.js
+
+// Implement saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Implement mondayWork function
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Implement wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
